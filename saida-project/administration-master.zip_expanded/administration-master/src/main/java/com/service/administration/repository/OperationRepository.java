5de682dc57342dcec21c8f42f362a112f7e8a1e2
RAKIBSaida <rakibsaida@gmail.com>
2020-02-16 19:59:22 +0000

    Delete OperationRepository.java
    Co-Authored-By: rakibsaida <rakibsaida@users.noreply.github.com>

--- saida-project/administration-master.zip_expanded/administration-master/src/main/java/com/service/administration/repository/OperationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.service.administration.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.service.administration.models.Operation;
-
-public interface OperationRepository extends JpaRepository<Operation, Long>{
-
-}
