01730db6e74732971c7a4f17d9d145cbdc9e6bfe
RAKIBSaida <rakibsaida@gmail.com>
2020-02-16 19:59:17 +0000

    Delete ContratRepository.java
    Co-Authored-By: rakibsaida <rakibsaida@users.noreply.github.com>

--- saida-project/administration-master.zip_expanded/administration-master/src/main/java/com/service/administration/repository/ContratRepository.java
+++
@@ -1,9 +0,0 @@
-package com.service.administration.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.service.administration.models.Contrat;
-
-public interface ContratRepository extends JpaRepository<Contrat, Long>{
-
-}
