63b3401273fa0e14c49cb971451ca964505f6eb5
RAKIBSaida <rakibsaida@gmail.com>
2020-02-16 19:41:44 +0000

    Create ServiceTransactionApplicationTests.java
    Co-Authored-By: rakibsaida <rakibsaida@users.noreply.github.com>

---
+++ saida-project/service-transaction/src/test/java/org/sid/servicetransaction/ServiceTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.servicetransaction;
+
+import org.junit.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ServiceTransactionApplicationTests {
+
+	@Test
+public 	void contextLoads() {
+	}
+
+}
